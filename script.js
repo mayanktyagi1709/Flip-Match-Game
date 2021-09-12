f964172a34986ca1108ec3daf74fbf8dc2f78382
Mayank Tyagi <mayanktyagi1709@gmail.com>
2021-09-12 21:01:03 +0530

    modified toggle

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
