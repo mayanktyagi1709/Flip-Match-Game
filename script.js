15361227f6844aebdd942daa7f7a1495dfb9db42
Mayank Tyagi <mayanktyagi1709@gmail.com>
2021-09-13 21:34:35 +0530

    refactored script

--- script.js
+++ script.js
@@ -10,23 +10,36 @@ function flipCard() {
     // first click
     hasFlippedCard = true;
     firstCard = this;
-  } else {
-    // second click
-    hasFlippedCard = false;
-    secondCard = this;
 
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        // it's a match!
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-      } else {
-        // not a match
-        setTimeout(() => {
-          firstCard.classList.remove('flip');
-          secondCard.classList.remove('flip');
-        }, 1500);
-      }
-  }
+    return;
+  } 
+
+  // second click
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
 }
+
+function checkForMatch()
+{
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disablecards()
+{
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipcards()
+{
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    econdCard.classList.remove('flip');
+  }, 1500);
+}
+        
 
 cards.forEach(card => card.addEventListener('click', flipCard));
