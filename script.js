81415394788d19370281196649b1e4c2131d032a
Mayank Tyagi <mayanktyagi1709@gmail.com>
2021-09-13 21:50:30 +0530

    added reset and shuffle

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let firstCard, secondCard;
 
 function flipCard() {
   if(lockBoard) return;
+  if(this === firstCard) return;
+
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
@@ -35,7 +37,7 @@ function disableCards()
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
-  lockBoard = false;
+  resetBoard();
 }
 
 function unflipCards()
@@ -44,8 +46,23 @@ function unflipCards()
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
 
-    lockBoard = false
+    resetBoard();
   }, 1500);
 }
+
+function resetBoard()
+{
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let ramdomPos = Math.floor(Math.random() * 12);
+    card.style.order = ramdomPos;
+  });
+})();
         
 cards.forEach(card => card.addEventListener('click', flipCard));
